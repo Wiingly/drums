27243afd87b2082a4805183032eb010fd9fb1a41
nickcabuno <nickcabuno17@gmail.com>
2021-08-25 18:18:06 -0400

    setting up for deploy

--- api/server.js
+++ api/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const cors = require('cors')
 const wingRouter = require('./wings/router');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors())
 
 server.use('/api/wings', wingRouter);
 
+server.get('api/wings', (req, res) => {
+    res.json({ message: 'api up' })
+})
+
 server.use('*', (req, res) => { 
-    res.json({ api: 'up' })
-  })
+    res.send(`<h1>Hello</h1>`)
+})
 
 module.exports = server;
