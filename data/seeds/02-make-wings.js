67d4e13bb517927a4678244dceb959257246337e
nickcabuno <nickcabuno17@gmail.com>
2021-08-25 16:39:50 -0400

    added data

--- data/seeds/02-make-wings.js
+++ data/seeds/02-make-wings.js
@@ -1,3 +1,14 @@
-exports.seed = function(knex) {
+const wings = [
+    { flavor: "24 strong",
+      amount: 12,
+      location: "Big Shot Bobs"
+    },
+    { flavor: "cash club",
+      amount: 12,
+      location: "Big Shot Bobs"
+    }
+]
 
+exports.seed = async function(knex) {
+    await knex('wings').insert(wings)
 }
