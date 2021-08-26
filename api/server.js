19a3e124dfbe22dacd77393efbe4593c5a8ecbe6
nickcabuno <nickcabuno17@gmail.com>
2021-08-26 01:03:21 -0400

    idk

--- api/server.js
+++ api/server.js
@@ -9,12 +9,4 @@ server.use(cors())
 
 server.use('/api/wings', wingRouter);
 
-server.get('/api/wings', (req, res) => { 
-    res.json({ message: '<h1>i hate heroku</h1>' })
-})
-
-server.use('*', (req, res) => { 
-    res.send('<h1>i hate heroku</h1>')
-})
-
 module.exports = server;
