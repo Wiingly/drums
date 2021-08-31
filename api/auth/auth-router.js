112b34677c3f5cea2a34a9cd17397610a684a9ba
nickcabuno <nickcabuno17@gmail.com>
2021-08-31 00:06:19 -0400

    bruhhhhhhhhhhhhhhh

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,16 +8,13 @@ const { jwtSecret } = require('../config/secrets.js');
 router.post('/register', mw.checkNewUserPayload, mw.formatNewUserPayload, mw.checkUsernameUnique, async (req, res, next) => {
     let user = req.body;
     const hash = await bcrypt.hashSync(user.password, 8);
-
     user.password = hash;
-
     try {
         const saved = await Users.add(user);
         res.status(201).json(saved);
     } catch (err) {
         next(err);
     }
-
 });
 
 router.post('/login', mw.checkLoginCredentials, mw.checkUsernameExists, (req, res, next) => {
